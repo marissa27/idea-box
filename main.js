441b143d7e6ce3bd58fe1f02a8f92bd793164b02
Marissa Reinke <reinke.marissa@gmail.com>
2016-12-19 13:53:02 -0700

    Setup local storage. Fix icon sizes. Add reset.css code.

--- main.js
+++ main.js
@@ -1,23 +1,29 @@
+$(document).ready(function(){
+    for(var i =0; i < localStorage.length; i++){
+      var object = JSON.parse(localStorage.getItem(localStorage.key(i)));
+      ideaContent(object);
+    }
+})
+
 // Creates idea properties
 function CreateIdea (title, body) {
   this.title = title;
   this.body = body;
-  }
+  this.id = Date.now();
+  this.quality = 'swill';
+}
 
-var i = 1;
 function ideaContent(createIdea) {
-  $("<article />", { "class":"ideas", id:"idea"+i })
-     .prepend(`<h5 contenteditable>${createIdea.title}</h5>
+  $(".stored").prepend(`<article id="${createIdea.id}"><h5 contenteditable>${createIdea.title}</h5><img class="icon" src="images/delete.svg"
      <p contenteditable>${createIdea.body}</p>
-     <button><</button><button>></button><p>Quality:</p><p class="quality"></p>`)
-     .prependTo($(".stored"));
-  i++;
-};
+     <img class="icon" src="images/upvote.svg"><img class="icon" src="images/downvote.svg"><p>Quality:</p><p class="quality"></p></article>`)
+     };
 
 // Sets idea in motion from user input and btn click
 $( ".save-btn" ).click(function() {
   var title = $( ".title" ).val();
   var body = $( ".body" ).val();
-  var createIdea = new CreateIdea(title, body);
-  ideaContent(createIdea);
+  var idea = new CreateIdea(title, body);
+  ideaContent(idea);
+  localStorage.setItem(idea.id, JSON.stringify(idea));
 });
