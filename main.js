5935a7456479cd0cbe76e91f1b17d7b0c19c1435
Marissa Reinke <reinke.marissa@gmail.com>
2016-12-19 15:56:42 -0700

    Get quality buttons to work

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-    for(var i =0; i < localStorage.length; i++){
+    for(var i = 0; i < localStorage.length; i++){
       var object = JSON.parse(localStorage.getItem(localStorage.key(i)));
       ideaContent(object);
     }
@@ -14,10 +14,14 @@ function CreateIdea (title, body) {
 }
 
 function ideaContent(createIdea) {
-  $(".stored").prepend(`<article id="${createIdea.id}"><h5 contenteditable>${createIdea.title}</h5><img class="icon" src="images/delete.svg"
+  $(".stored").prepend(`<article class="article" id="${createIdea.id}"><h5 contenteditable>${createIdea.title}</h5><button class="delete-btn">D</button>
      <p contenteditable>${createIdea.body}</p>
-     <img class="icon" src="images/upvote.svg"><img class="icon" src="images/downvote.svg"><p>Quality:</p><p class="quality"></p></article>`)
-     };
+     <button class="up">UP</button>
+     <button class="down"></button>
+     <p class="quality">Quality:</p>
+     <p class="type">${createIdea.quality}</p>
+     </article>`)
+    };
 
 // Sets idea in motion from user input and btn click
 $( ".save-btn" ).click(function() {
@@ -27,3 +31,25 @@ $( ".save-btn" ).click(function() {
   ideaContent(idea);
   localStorage.setItem(idea.id, JSON.stringify(idea));
 });
+
+$('.stored').on('click', '.delete-btn', function(){
+  $(this).closest('.article').remove();
+})
+
+$( '.stored' ).on('click', '.up', function() {
+  var qualityType = $(this).siblings('.type');
+  if(qualityType.text() === 'swill'){
+    qualityType.text('plausible');
+  } else if(qualityType.text() === 'plausible') {
+      qualityType.text('genius');
+    }
+})
+
+$( '.stored' ).on('click', '.down', function() {
+  var qualityType = $(this).siblings('.type');
+  if(qualityType.text() === 'genius'){
+    qualityType.text('plausible');
+  } else if(qualityType.text() === 'plausible') {
+      qualityType.text('swill');
+    }
+})
